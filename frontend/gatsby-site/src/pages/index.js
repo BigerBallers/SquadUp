06b06b6ae1c4c55dd023e1c65169ea6c0a2f2d42
Sean Woods <srwoods@ucsc.edu>
2018-07-11 13:13:36 -0800

    Updated files and added event page

--- frontend/gatsby-site/src/pages/index.js
+++ frontend/gatsby-site/src/pages/index.js
@@ -6,7 +6,8 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/"><p>Go to page 2</p></Link>
+	<Link to="/event_page/"><p>Go to Events</p></Link>
   </div>
 )
 
