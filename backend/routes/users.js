e8a857dd11035005b521e914de612515b3c7aa53
Zahir Yahya <theeYahya@gmail.com>
2018-07-12 13:58:45 -0700

    need to test if isAuthenticated works

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -23,4 +23,45 @@ router.post('/auth/google', googleToken, function(req, res, next) {
 }, generateToken, sendToken);
 
 
+router.get('userData:id', function(req, res, next){
+	console.log('query: ', req.query);
+	console.log('id: ', id);
+
+	res.send("userData");
+});
+
+
+
+router.post('/login', googleToken, function(req, res, next) {
+  if (!req.user) {
+  	console.log('not authenticated');
+    return res.send(401, 'User Not Authenticated');
+  }
+  req.auth = {
+    id: req.user.id
+  };
+  console.log('req.user: ', req.user);
+  next();
+});
+
+
+router.post('/test', function(req, res) {
+	res.send('nothing');
+});
+
+router.get('/test', checkAuthentication, function(req, res) {
+	res.send({ express: 'Hello From Express' });
+});
+
+
+function checkAuthentication(req,res,next){
+    if(req.isAuthenticated()){
+        //req.isAuthenticated() will return true if user is logged in
+        console.log('is authenticated!');
+        next();
+    } else{
+        console.log('is not authenticated');
+    }
+}
+
 module.exports = router;
