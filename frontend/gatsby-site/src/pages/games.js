e8ea0d0cd168cc9292fd85bc864eb45915c0a4ca
skhan17 <shyaankhan55@gmail.com>
2018-07-13 20:03:53 -0700

    adding map

--- frontend/gatsby-site/src/pages/games.js
+++ frontend/gatsby-site/src/pages/games.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import Link from 'gatsby-link'
 import { compose, withProps } from "recompose";
 import {
@@ -8,14 +8,14 @@ import {
   Marker
 } from "react-google-maps";
 
-const eventPage = compose(
+const MyMapComponent = compose(
   withProps({
     /**
      * Note: create and replace your own key in the Google console.
      * https://console.developers.google.com/apis/dashboard
      * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
      */
-    googleMapURL:
+    /*googleMapURL:
       "https://maps.googleapis.com/maps/api/js?key=AIzaSyBgne_-KxLx1Sbd2CHtT7EklGSPAyjXH5I&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
@@ -24,15 +24,15 @@ const eventPage = compose(
   withScriptjs,
   withGoogleMap
 )(props => (
-  <div>
-    <h1>Hi from Events</h1>
-    <p>Welcome to the Event Page</p>
-    <Link to="/">Go back to the homepage</Link>
-  <GoogleMap defaultZoom={13} defaultCenter={{ lat: 36.983127, lng:-122.051707 }}>
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
     {props.isMarkerShown && (
-      <Marker position={{ lat: 36.983127, lng: -122.051707}} />
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
     )}
   </GoogleMap>
+  <div>
+  <p>
   </div>
 ));
-export default eventPage
+
+export default MyMapComponent
+*/
