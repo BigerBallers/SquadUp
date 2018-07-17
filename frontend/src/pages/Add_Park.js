876ec243e7f24102533a7dc5b39008462bf01505
Chuangbo Tong <tongchuangbo@college9-169-233-122-46.resnet.ucsc.edu>
2018-07-16 20:40:49 -0700

    Add park front & backend

--- frontend/src/pages/Add_Park.js
+++ frontend/src/pages/Add_Park.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './Add_Park_Style.css';
 import Link from "gatsby-link";
+import Geocode from "react-geocode";
 
+Geocode.setApiKey("AIzaSyDAqgkDUgbqZuBZbDXkiaXubQWvdV3gYZg");
+  
+Geocode.enableDebug();
 class Add_Park extends Component {
   constructor(props) { //constructor of props and states
     super(props);
@@ -15,6 +19,25 @@ class Add_Park extends Component {
     this.handleSports=this.handleSports.bind(this);
 
   }
+
+  sendParkData(data){
+    fetch('http://localhost:8080/parks/addPark', {
+      method: 'POST',
+      dataType: 'json',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': sessionStorage.getItem('token')
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => console.log('parsing failed', error))
+  }
+  
 
   //functions/methods that receive inputs
 
@@ -34,20 +57,54 @@ class Add_Park extends Component {
     })
   }
   handleSubmit(event){
+
     const {park_name, park_location, selector}=this.state
-    alert("Park Name: "+this.state.park_name
-      +"\nPark Location: "+this.state.park_location
-      +"\nSports: "+this.state.selector+
-      "\nYou are all set!\n ");
+    // alert("Park Name: "+this.state.park_name
+    //   +"\nPark Location: "+this.state.park_location
+    //   +"\nSports: "+this.state.selector+
+    //   "\nYou are all set!\n ");
     //here an alert window is popped up
     this.setState({
       park_name: '',
       park_location: '',
       selector: ''
-    })
-    
+    }) 
+    var geo = this.convertGeo(this.state.park_location);
+    console.log("geo: ", geo);
+    if (geo!=[]){
 
+      
+    }
+    
   }
+
+  convertGeo(input){
+    var geo;
+    Geocode.fromAddress(input).then(
+      response => {
+
+        const { lat, lng } = response.results[0].geometry.location;
+        geo = [lat, lng];
+        console.log(geo);
+        var data = {
+        name: this.state.park_name,
+        address: this.state.park_location,
+        description:"awesome",
+        sports:this.state.selector,
+        geo: this.geo
+      };
+      this.sendParkData(data);
+        return geo;
+
+      },
+      error => {
+        console.error(error);
+        alert("Not Valid Address!");
+        return [];
+      }
+    );
+  }
+
 
   render(){
     const {park_name, park_location, selector}=this.state
