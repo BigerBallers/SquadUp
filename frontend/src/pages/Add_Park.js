59e00ed7fe8f874f6aed585c29af495a32046ae7
Chuangbo Tong <tongchuangbo@eduroam-169-233-243-242.ucsc.edu>
2018-07-19 10:39:15 -0700

    updated add park view

--- frontend/src/pages/Add_Park.js
+++ frontend/src/pages/Add_Park.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import './Add_Park_Style.css';
 import Link from "gatsby-link";
-import Geocode from 'react-geocode';
+import Geocode from "react-geocode";
 
-Geocode.setApiKey("AIzaSyDAqgkDUgbqZuBZbDXkiaXubQWvdV3gYZg");
+Geocode.setApiKey("AIzaSyDAqgkDUgbqZuBZbDXkiaXubQWvdV3gYZg");  
 Geocode.enableDebug();
-
 
 class Add_Park extends Component {
   constructor(props) { //constructor of props and states
     super(props);
-
+    
     this.state = { //three fields for add parK: name, locaiton and sport
       park_name: '',
       park_location: '',
-      selector: '',
+      selector: [],
+      park_description: '',
     }
-    this.handleSubmit= this.handleSubmit.bind(this);
+    this.handleSubmit= this.handleSubmit.bind(this); 
     this.handleSports=this.handleSports.bind(this);
+    this.handleDescription=this.handleDescription.bind(this);
 
   }
-
 
   sendParkData(data){
     fetch('http://localhost:8080/parks/addPark', {
@@ -39,7 +39,9 @@ class Add_Park extends Component {
     })
     .catch(error => console.log('parsing failed', error))
   }
+  
 
+  //functions/methods that receive inputs
 
   handleParknameChange(event){
     this.setState({
@@ -51,18 +53,29 @@ class Add_Park extends Component {
       park_location: event.target.value
     })
   }
-  handleSports(event){
+  handleSports(selector){
     this.setState({
-      selector: event.target.value
+      selector: selector.target.value
     })
   }
-
-
-  handleSubmit(event){
-    const {park_name, park_location, selector}=this.state
-    this.convertGeo(this.state.park_location);
+  handleDescription(event){
+    console.log("description: ", event);
+    this.setState({
+      park_description: event.target.value
+    })
   }
+  handleSubmit(event){
 
+    const {park_name, park_location, selector, park_description}=this.state
+    alert("Park Name: "+this.state.park_name
+      +"\nPark Location: "+this.state.park_location
+      +"\nSports: "+this.state.selector+
+      "\nYou are all set!\n ");
+    //here an alert window is popped up
+
+    this.convertGeo(this.state.park_location);   
+ 
+  }
 
   convertGeo(input){
     var geo;
@@ -71,58 +84,55 @@ class Add_Park extends Component {
 
         const { lat, lng } = response.results[0].geometry.location;
         geo = [lng, lat];
-
+        console.log(geo);
         var data = {
         name: this.state.park_name,
         address: this.state.park_location,
-        description:"awesome",
+        description: this.state.park_description,
         sports:[this.state.selector],
         geo: geo
       };
-
-      // after we get lnt lat we add park to database
+      
+      console.log('data: ', data);
       this.sendParkData(data);
+      return geo;
 
-      // alert after park is added
-      alert("Park Name: "+this.state.park_name
-           +"\nPark Location: "+this.state.park_location
-           +"\nSports: "+this.state.selector+
-           "\nYou are all set!\n ");
       },
       error => {
         console.error(error);
         alert("Not Valid Address!");
+        return [];
       }
     );
   }
 
+
   render(){
-    const {park_name, park_location, selector}=this.state
-    const enabled=
+    const {park_name, park_location, selector, park_description}=this.state
+    const enabled= 
       park_name.length > 0 &&
-      park_location.length>0 &&
+      park_location.length>0 && 
       selector.length>0; //unable the sumbit button when no input
 
     return (
       <body>
+
+      <div className="total">
+        
         <div className="MyHeader">
           <h2>Add Park</h2>
-            <div className="back-button">
-             <Link to="/">
-              <button class="btn btn-info btn-lg" role="button">Back</button>
-             </Link>
-             //Need to refresh after clicking, don't know why
-            </div>
+            
         </div>
 
         <div className="forms">
           <div className="ParkName">
             Park Name
           </div>
+          <div className="decoration"></div>
             <div className="name_field">
-              <input value={this.state.park_name}
-              onChange={this.handleParknameChange.bind(this)}
-              placeholder="e.g. Rucker Park?"
+              <input value={this.state.park_name} 
+              onChange={this.handleParknameChange.bind(this)} 
+              placeholder="e.g. Rucker Park" 
               style={{width: "100%", height:"100%"}}
               required />
             </div>
@@ -138,8 +148,8 @@ class Add_Park extends Component {
               style={{width: "100%", height:"100%"}} required />
             </div>
           <div className="picker">
-            <select onChange={this.handleSports} value={this.state.selector} style={{width: "50%", height:"100%"}}>
-              <option value="" disabled selected>Which Sport?</option>
+            <select value={this.state.selector} onChange={this.handleSports} style={{width: "50%", height:"100%"}}> 
+              <option value="" disabled selected>Sport?</option>
               <option value="Basketball">Basketball</option>
               <option value="Soccer">Soccer</option>
               <option value="Frisbee">Frisbee</option>
@@ -147,12 +157,20 @@ class Add_Park extends Component {
               <option value="Baseball">Baseball</option>
             </select>
           </div>
-
+          <div className="description">
+            <textarea 
+            rows="10"
+            cols="50"
+            value={this.state.park_description}
+            onChange={this.handleDescription.bind(this)}
+            placeholder="Tell us something about the park!" 
+            required />
+          </div>
           <div className="submit-button">
             <button onClick={this.handleSubmit} disabled={!enabled}>Submit</button>
           </div>
         </div>
-
+      </div>
       </body>
       );
   }
