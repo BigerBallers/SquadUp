71e2261bf1f06b2e91ecd841f65b9161bb9ff023
Chuangbo Tong <tongchuangbo@eduroam-169-233-243-242.ucsc.edu>
2018-07-23 16:08:25 -0700

    event_page added

--- frontend/src/pages/event_page.js
+++ frontend/src/pages/event_page.js
@@ -1,38 +1,99 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import { compose, withProps } from "recompose";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+import React, { Component } from 'react';
+import './event_page.css';
+import Link from "gatsby-link";
+import Geocode from "react-geocode";
 
-const eventPage = compose(
-  withProps({
-    /**
-     * Note: create and replace your own key in the Google console.
-     * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
-     */
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBgne_-KxLx1Sbd2CHtT7EklGSPAyjXH5I&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%`, width: '50%'}} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <div>
-    <h1>Hi from Events</h1>
-    <p>Welcome to the Event Page</p>
-    <Link to="/">Go back to the homepage</Link>
-  <GoogleMap defaultZoom={13} defaultCenter={{ lat: 36.983127, lng:-122.051707 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: 36.983127, lng: -122.051707}} />
-    )}
-  </GoogleMap>
-  </div>
-));
-export default eventPage
+class View_Event extends Component {
+  constructor(props) { //constructor of props and states
+    super(props);
+    
+  }
+
+  //part of fetching
+  componentDidMount(){
+    this.fetchData();
+  }
+
+   //fetches park data from db. should not work at first because no access token
+  fetchData(){
+    fetch('http://localhost:8080/events', {
+      method: 'get',
+      dataType: 'json',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': sessionStorage.getItem('token')
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => console.log('parsing failed', error))
+  }
+
+
+  render(){
+
+    return (
+      <body>
+
+        <div className="centered-container">
+
+          <div className="title">
+            3v3 Tournament
+          </div>
+
+          <div className="category">
+            <h3>Basketball</h3>
+          </div>
+
+          <div className="host-card">
+            <div className="host-card-title">
+              <h3> Hosted by: </h3>
+            </div>
+            <div className="host-card-pic">
+              <img src="test.jpg" alt="test"/>
+            </div>
+            <div className="host-card-name">
+              <h4> Zahir Yahya </h4>
+            </div>
+          </div>
+
+          <h3>Location:</h3>
+          <p>Random Park
+            <br />
+            555 Somthing st,
+            Santa Cruz CA
+          </p>
+
+          <h3>Date: </h3>
+          <p>July 5th<br />
+          Start: 5:00pm End: 6:00pm</p>
+
+           <div className="description">
+            <h3>Description: </h3>
+            <p>Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla 
+            </p>
+        </div>
+
+        <h3>Attending (3 out of 12): </h3>
+
+        <div>
+          <ul>
+            <li>Person 1</li>
+            <li>Person 2</li>
+            <li>Person 3</li>
+          </ul>
+        </div>
+
+        <button type="button">Join!</button>
+
+        </div>
+
+      </body>
+      );
+  }
+}
+
+export default View_Event;
